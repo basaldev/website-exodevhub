8f02ba3753c9a2c18ba42b0928e01b2d3fc42f04
Alex King <alex@exolever.com>
2019-01-16 15:20:24 +0900

    Added CMS backend

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
+    'gatsby-plugin-netlify-cms',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
