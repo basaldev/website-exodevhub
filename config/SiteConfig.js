f509a8cacba0dd2d898b305923cbe09b531c5785
Alex King <alexanderddking@gmail.com>
2019-02-04 13:04:12 +0900

    Remove trailing slash

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -3,7 +3,7 @@ module.exports = {
 
   siteTitle: 'ExO DevHub', // Navigation and Site Title
   siteTitleAlt: 'ExO DevHub', // Alternative Site title for SEO
-  siteUrl: 'https://www.exodevhub.com/', // Domain of your site. No trailing slash!
+  siteUrl: 'https://www.exodevhub.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
