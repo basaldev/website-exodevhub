4bb4d1b4d3568887a60a5e09ed0fa9368ca67d47
Alex King <alexanderddking@gmail.com>
2019-03-28 11:20:40 +0900

    Added theme color for chrome

--- src/html.tsx
+++ src/html.tsx
@@ -20,6 +20,8 @@ export default class HTML extends React.Component<Props, {}> {
 
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta name="theme-color" content="#000000" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
