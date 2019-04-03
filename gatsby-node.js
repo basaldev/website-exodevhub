94552c640b70c26a031c1170e5ac9c0ec1e6c834
tomrbowden <tom.bowden@gmail.com>
2019-04-03 15:43:32 +0900

    Clean up gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,50 +6,37 @@ function createSlug(node, key, createNodeField) {
     Object.prototype.hasOwnProperty.call(node, 'frontmatter') &&
     Object.prototype.hasOwnProperty.call(node.frontmatter, key)
   ) {
-  createNodeField({ node, name: 'slug', value: `/${_.kebabCase(node.frontmatter[key])}`})
+    createNodeField({
+      node,
+      name: 'slug',
+      value: `/${_.kebabCase(node.frontmatter[key])}`,
+    })
+  }
 }
-}
-function createPostType(postType, rawQuery, component, createPage, contextCallback){
-  let result  = []
-
-        _.each(rawQuery, edge => {
-            if(_.get(edge, 'node.frontmatter.type') === postType){
-              result = result.concat(edge);
-            }
-        })
-        result = _.uniq(result)
-        result.forEach((edge, index) => {
-          const extraContext = contextCallback(result, index) || {};
-
-          createPage({
-            path: edge.node.fields.slug,
-            component,
-            context: {
-              slug: edge.node.fields.slug,
-              ...extraContext
-            },
-          })
-        })
-}
-function createTaxonomy(name, postType, rawQuery, component, createPage, contextCallback) {
+function createPostType(
+  postType,
+  rawQuery,
+  component,
+  createPage,
+  contextCallback
+) {
   let result = []
 
   _.each(rawQuery, edge => {
-    if (_.get(edge, 'node.frontmatter.type') === postType && edge.node.frontmatter[name]) {
-      result = result.concat(edge.node.frontmatter[name])
+    if (_.get(edge, 'node.frontmatter.type') === postType) {
+      result = result.concat(edge)
     }
   })
-
   result = _.uniq(result)
+  result.forEach((edge, index) => {
+    const extraContext = contextCallback(result, index) || {}
 
-  result.forEach((single, index) => {
-    const extraContext = contextCallback(result, index) || {};
     createPage({
-      path: `/${name}/${_.kebabCase(single)}`,
+      path: edge.node.fields.slug,
       component,
       context: {
-        single,
-        ...extraContext
+        slug: edge.node.fields.slug,
+        ...extraContext,
       },
     })
   })
@@ -58,11 +45,7 @@ function createTaxonomy(name, postType, rawQuery, component, createPage, context
 exports.onCreateNode = ({ node, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === 'MarkdownRemark') {
-    const slugFields = [
-      'slug',
-      'title',
-      'fullName'
-    ];
+    const slugFields = ['slug', 'title', 'fullName']
     _.map(slugFields, key => createSlug(node, key, createNodeField))
   }
 }
@@ -96,24 +79,25 @@ exports.createPages = ({ graphql, actions }) => {
           console.log(result.errors)
           reject(result.errors)
         }
-        const results = result.data.posts.edges;
+        const results = result.data.posts.edges
 
         createPostType('post', results, postPage, createPage, (r, i) => {
           return {
             next: i === 0 ? null : r[i - 1].node,
-            prev: i === r.length - 1 ? null : r[i + 1].node
+            prev: i === r.length - 1 ? null : r[i + 1].node,
           }
-        });
-        createPostType('person', results, path.resolve(`src/templates/person.tsx`), createPage, (r, i) =>  {});
-        // WIP
-        // createTaxonomy('category', 'post', results, categoryPage, createPage, (r, i) => {
-        //   return {}
-        // });
+        })
+        createPostType(
+          'person',
+          results,
+          path.resolve(`src/templates/person.tsx`),
+          createPage
+        )
         let categories = []
 
         _.each(results, edge => {
           if (!_.isNull(_.get(edge, 'node.frontmatter.category'))) {
-              categories = categories.concat(edge.node.frontmatter.category)
+            categories = categories.concat(edge.node.frontmatter.category)
           }
         })
         categories = _.uniq(categories)
